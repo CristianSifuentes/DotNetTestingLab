b0172552beaa68b461d9375553939254e8b73755
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2026-06-19 12:36:14 -0500

    Adding new code in StringOperationsTest

--- StringManipulation.Tests/StringOperationsTest.cs
+++ StringManipulation.Tests/StringOperationsTest.cs
@@ -14,9 +14,39 @@ namespace StringManipulation.Tests
         {
             var strOperations = new StringOperations();
 
+            // Act
             var result = strOperations.ConcatenateStrings("Hello", "World");
 
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
             Assert.Equal("Hello World", result);
+        }
+
+        [Fact]
+        public void IsPalindrome_True()
+        {
+            // Arrange
+            var strOperations = new StringOperations();
+
+            // Act
+            var result = strOperations.IsPalindrome("ama");
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsPalindrome_False()
+        {
+            // Arrange
+            var strOperations = new StringOperations();
+
+            // Act
+            var result = strOperations.IsPalindrome("hello");
+
+            // Assert
+            Assert.False(result);
         }
     }
 }
